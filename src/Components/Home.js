5a6f981dee52936f696645f76c9e9cb09b08d79f
yingjie <yingjie_qiao@outlook.com>
2021-07-24 15:58:28 +0800

    homepage except techstack done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import { FaEnvelope, FaLinkedin, FaGithub, FaCodepen } from "react-icons/fa";
 
 import styles from "../css/home.module.css";
 import data from "./data";
@@ -9,6 +10,7 @@ class Home extends Component {
     render() {
         return (
             <div className={styles.home}>
+
                 <div className={styles.header_container}>
                     <Link to="/"><h1>{data.fullname}</h1></Link>
                     <nav>
@@ -19,20 +21,65 @@ class Home extends Component {
                     </nav>
                 </div>
 
-                <div className={styles.header_container}>
-                    <div className={styles.home_container}>
-                        <div className={styles.home_head_wrapper}>
-                            <h1> Hello there! <br></br> I'm <span>{data.name}</span></h1>
+                <div className={styles.home_container}>
+                    <div className={styles.home_head_wrapper}>
+                        <h1> Hello there! <br></br> I'm <span>{data.name}</span></h1>
 
-                            {/* <p>{paragraph}</p> */}
-                            
-                            </div>
-                            <div className={styles.image_container}>
-                                <img src="me.png" alt="Author"></img>
-                                <div className={styles.bg}></div>
-                            </div>
+                        <p>{data.intro}</p>
+                        
+                        </div>
+                        <div className={styles.image_container}>
+                            <img src={data.me_img} alt="Author"></img>
+                            <div className={styles.bg}></div>
+                        </div>
+                </div>
+
+                <div className={styles.contact_container}>
+                    
+                    <p>{data.paragraph1}</p>
+
+                    {/* techstack */}
+                    {/* <br></br>
+                    <br></br> */}
+
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <h1>Get in touch.</h1>
+
+                    <br></br>
+                    <br></br>
+
+                    <a className={styles.email_link} href={`mailto:${data.contact_email}`}>
+                    {data.contact_email}
+                    </a>
+
+                    <div className={styles.social_links}>
+                        <ul>
+                            <li>
+                                <a href={`mailto:${data.contact_email}`}>
+                                    <FaEnvelope></FaEnvelope>
+                                </a>
+                            </li>
+                            <li>
+                                <a href={data.contact_link[0].url}>
+                                    <FaLinkedin></FaLinkedin>
+                                </a>
+                            </li>
+                            <li>
+                                <a href={data.contact_link[1].url}>
+                                    <FaGithub></FaGithub>
+                                </a>
+                            </li>
+
+                        </ul>
                     </div>
                 </div>
+
+
             </div>
 
             
--- src/Components/data.js
+++ src/Components/data.js
@@ -1,4 +1,24 @@
 export default {
     fullname: "Yingjie Qiao",
-    name: "Yingjie"
+    name: "Yingjie",
+    me_img: "me.png",
+    intro:     "I'm a Software Engineer. \
+        Currently a Final Year Computer Science student at Singapore University of Technology and Design (SUTD),  \
+        and working as a fullstack Software Engineer Intern at SAP.",
+    paragraph1:  "I aspire to build high-impact software products, \
+                with attention to code details and scalable & maintainable architecture. \
+                I enjoy solving problems with technology, and I constantly engage myself to new challenges \
+                and to learn new skills. I believe constant self-learning is essential for a software engineer to \
+                keep up with the fast-evolving technologies and excel.",
+    contact_email: "yingjie_qiao@outlook.com",
+    contact_link: [
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/yingjie-qiao/"
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/YingjieQiao"
+        }
+    ]
 }
